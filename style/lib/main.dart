649dfea14860f01d7811383cdd45df1c5d208cf0
fetty athiyatul maula <fetty.maula98@gmail.com>
2024-12-10 17:02:19 +0700

    card and parsing data

--- style/lib/main.dart
+++ style/lib/main.dart
@@ -10,61 +10,59 @@ void main() {
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: const Color.fromARGB(255, 235, 233, 213),
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 184, 13, 27),
-        leading: Icon(
-          Icons.home,
-          color: const Color.fromARGB(255, 240, 241, 245),
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Card & Parsing",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        title: Center(
-          child: Text(
-            "IDR Corner",
-            style: TextStyle(color: Colors.white, fontSize: 15),
-          ),
-        ),
-        actions: [
-          Icon(
-            Icons.search,
-            color: Colors.white,
-          )
-        ],
       ),
       body: Container(
-          child: Column(
-        children: [
-          Icon(
-            Icons.local_pizza,
-            size: 70.0,
-            color: Colors.red,
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.donut_large,
-                size: 70.0,
-                color: Colors.red,
-              ),
-              Icon(
-                Icons.donut_large,
-                size: 70.0,
-                color: Colors.red,
-              ),
-              Icon(
-                Icons.donut_large,
-                size: 70.0,
-                color: Colors.red,
-              ),
-            ],
-          ),
-          Icon(
-            Icons.cake,
-            size: 70.0,
-            color: Colors.red,
-          )
-        ],
-      )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CardSaya(icon: Icons.home, warnaIcon: Colors.brown, teks: "Home"),
+            CardSaya(
+                icon: Icons.favorite, warnaIcon: Colors.pink, teks: "Favorite"),
+            CardSaya(icon: Icons.place, warnaIcon: Colors.blue, teks: "Place"),
+            CardSaya(
+                icon: Icons.settings, warnaIcon: Colors.black, teks: "Setting"),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class CardSaya extends StatelessWidget {
+  const CardSaya({
+    super.key,
+    required this.icon,
+    required this.teks,
+    required this.warnaIcon,
+  });
+
+  final IconData icon;
+  final String teks;
+  final Color warnaIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+      children: [
+        Padding(padding: EdgeInsets.all(10)),
+        Icon(
+          icon,
+          size: 50.0,
+          color: warnaIcon,
+        ),
+        Text(
+          teks,
+          style: TextStyle(fontSize: 10.0, color: Colors.brown),
+        )
+      ],
+    ));
   }
 }
