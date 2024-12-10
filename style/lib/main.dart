701dbb846e7f1af610016a3b6f1e215b16200538
fetty athiyatul maula <fetty.maula98@gmail.com>
2024-12-10 16:25:57 +0700

    AppBar

--- style/lib/main.dart
+++ style/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
+    title: "idrcorner application",
     home: new HalamanSatu(),
   ));
 }
@@ -10,18 +11,26 @@ class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: new Center(
-      child: new Container(
-        color: Colors.blue,
-        width: 200.0,
-        height: 100.0,
-        child: new Center(
-            child: new Icon(
-          Icons.android,
-          color: Colors.yellow,
-          size: 70.0,
-        )),
+      backgroundColor: const Color.fromARGB(255, 199, 217, 240),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 50, 52, 226),
+        leading: Icon(
+          Icons.home,
+          color: const Color.fromARGB(255, 240, 241, 245),
+        ),
+        title: Center(
+          child: Text(
+            "IDR Corner",
+            style: TextStyle(color: Colors.white, fontSize: 15),
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          )
+        ],
       ),
-    ));
+    );
   }
 }
